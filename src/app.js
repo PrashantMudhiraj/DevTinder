31d7610339c950bb39c9614796f943bfc6869d25
PrashantMudhiraj <prashantchevula@gmail.com>
2024-11-16 23:08:59 +0530

    expressions , regex, grouping,id's

--- src/app.js
+++ src/app.js
@@ -5,8 +5,14 @@ const app = express();
 
 //Below route will not allow other routes why because each and every route will match "/" path
 //code is executed in sequence 
-app.get("/dashboard", (req, res) => {
+app.get("/user", (req, res) => {
+    console.log(req.query)
     res.send("Hello this is dashboard");
+});
+
+app.get("/userId/:id", (req, res) => {
+    console.log(req.params)
+    res.send("Hello this is get id");
 });
 
 app.get("/test", (req, res) => {
@@ -21,6 +27,35 @@ app.delete("/user", (req, res) => {
     res.send('deleted successfully')
 })
 
+//expressions
+app.get("/ab?c", (req, res) => {
+    res.send('expression /ab?c')
+})
+
+app.get("/ab+c", (req, res) => {
+    res.send('expression /ab+c')
+})
+app.get("/ab*cd", (req, res) => {
+    res.send('expression /ab*cd')
+})
+
+//grouping
+app.get("/a(bc)?d", (req, res) => {
+    res.send('expression /ab*cd')
+})
+
+app.get("/a(bc)+d", (req, res) => {
+    res.send('expression /ab*cd')
+})
+
+//regex 
+app.get(/a/, (req, res) => {
+    res.send('regex expression valdation')
+})
+
+app.get(/.*fly$/, (req, res) => {
+    res.send('regex expression valdation')
+})
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000");
 });
