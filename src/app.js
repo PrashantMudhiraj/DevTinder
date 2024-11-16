3e8992fad4237b9a09ee2ab6eab9452be29afe45
PrashantMudhiraj <prashantchevula@gmail.com>
2024-11-16 22:45:01 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -2,13 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello this is dashboard");
-});
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
+
+//Below route will not allow other routes why because each and every route will match "/" path
+//code is executed in sequence 
+app.get("/dashboard", (req, res) => {
+    res.send("Hello this is dashboard");
 });
 
+app.get("/test", (req, res) => {
+    res.send("Hello from the server");
+});
+
+app.post("/user", (req, res) => {
+    res.send({ "name": "Prashant" })
+})
+
+app.delete("/user", (req, res) => {
+    res.send('deleted successfully')
+})
+
 app.listen(3000, () => {
-  console.log("Server is successfully listening on port 3000");
+    console.log("Server is successfully listening on port 3000");
 });
