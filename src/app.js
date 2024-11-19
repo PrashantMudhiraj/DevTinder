ebdde7d388d9e9fffbea86a52441de49c9b4c9e5
PrashantMudhiraj <prashantchevula@gmail.com>
2024-11-19 05:56:43 +0530

    dynamic signup

--- src/app.js
+++ src/app.js
@@ -5,18 +5,21 @@ const { adminAuth, userAuth, errorHandler } = require("./middlewares");
 const User = require("./model/user");
 
 const app = express();
+app.use(express.json())
 
 app.post("/signup", async (req, res) => {
     try {
-        const userObj = {
-            firstName: "Prashant",
-            lastName: "Chevula",
-            age: 25,
-            password: "1234567890",
-            emailId: "xyz@gmail.com",
-        };
+        // console.log(req.body)
+        // const userObj = {
+        //     firstName: "Prashant",
+        //     lastName: "Chevula",
+        //     age: 25,
+        //     password: "1234567890",
+        //     emailId: "xyz@gmail.com",
+        // };
         //Creating a new instance of the User model
-        const user = new User(userObj);
+        // const user = new User(userObj);
+        const user = new User(req.body)
         await user.save();
         res.send("signed up");
     } catch (error) {
