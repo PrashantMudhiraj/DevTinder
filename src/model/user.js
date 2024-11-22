2457a1b4450c6561b840df3c0ab251248f02c863
PrashantMudhiraj <prashantchevula@gmail.com>
2024-11-22 19:18:38 +0530

    schema update

--- src/model/user.js
+++ src/model/user.js
@@ -3,15 +3,21 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
     firstName: {
         type: String,
+        required: true,
     },
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
@@ -19,6 +25,16 @@ const userSchema = new Schema({
     gender: {
         type: String,
     },
+    photoUrl: {
+        type: String
+    },
+    about: {
+        type: String,
+        default: "about feild (Test)"
+    },
+    skills: {
+        type: [String]
+    }
 });
 
 // const userModel = model("User", userSchema);
